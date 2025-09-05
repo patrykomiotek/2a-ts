bbdd65afa7a8de29a55b428c5da38a2b6a8d32f4
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-05 16:05:40 +0200

    with barrels

--- src/shop/index.js
+++ src/shop/index.js
@@ -1,36 +1,4 @@
-import { Product } from "./Product.js";
-import { Shop } from "./Shop.js";
-import { Cart } from "./Cart.js";
-
-const shop = new Shop();
-const product1 = new Product({
-  id: 1,
-  name: "Klawiatura",
-  price: 123,
-});
-const product2 = new Product({
-  id: 2,
-  name: "Myszka",
-  price: 12,
-});
-const product3 = new Product({
-  id: 3,
-  name: "Monitor",
-  price: 678,
-});
-
-shop.addProduct(product1);
-shop.addProduct(product2);
-shop.addProduct(product3);
-shop.listProducts();
-
-const cart = new Cart();
-cart.addProduct(product1);
-cart.addProduct(product3);
-console.log("Basket items: ");
-cart.listProducts();
-console.log(cart.sumAmount());
-cart.removeProduct(1);
-console.log("Basket items: ");
-cart.listProducts();
-console.log(cart.sumAmount());
+// barrels
+export { Cart } from "./Cart";
+export { Shop } from "./Shop";
+export { Product } from "./Product";
