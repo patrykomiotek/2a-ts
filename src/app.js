51ee0f0cca9fbdb812516de5bcfeba99efc81151
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-19 15:56:30 +0200

    feat: use post endpoint
    feat: with render function
    
    feat: hide token

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const TOKEN = "";
+
 window.addEventListener("DOMContentLoaded", async () => {
   console.log("Hello");
 
@@ -46,13 +48,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
     console.log(data); //
 
-    const root = document.getElementById("root");
-    const characters = data.results;
-    characters.forEach((element) => {
-      const characterHtml = document.createElement("div");
-      characterHtml.innerHTML = `<h2>${element.name}</h2><div><img src="${element.image}" /></div>`;
-      root.appendChild(characterHtml);
-    });
+    render(data.results);
   } catch (error) {
     console.error(error);
   } finally {
@@ -61,12 +57,57 @@ window.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
+const fetchData = async () => {
+  const response = await fetch(
+    "https://api.airtable.com/v0/appHETENHL4rmOb9N/characters",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-type": "application/json",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+const render = (data) => {
+  const root = document.getElementById("root");
+  const characters = data;
+  characters.forEach((element) => {
+    const characterHtml = document.createElement("div");
+    characterHtml.innerHTML = `<h2>${element.name}</h2><div><img src="${element.image}" /></div>`;
+    root.appendChild(characterHtml);
+  });
+};
+
 const form = document.getElementById("main-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nameValue = document.getElementById("name").value;
   const statusValue = document.getElementById("status").value;
+
+  // add record to airtable
+  fetch("https://api.airtable.com/v0/appHETENHL4rmOb9N/characters", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      records: [
+        {
+          fields: {
+            name: nameValue,
+            status: statusValue,
+          },
+        },
+      ],
+    }),
+  });
 
   console.log({ nameValue, statusValue });
 });
